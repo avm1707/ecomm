61b11fdbf3020d5180f987f9483e352b4dc0746a
avm1707 <amanmishra9444@gmail.com>
2026-03-20 15:10:56 +0530

    add

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -93,7 +93,7 @@ class Navbar extends Component {
             <ul className="navbar-nav align-items-center">
               <li className="nav-item ml-5">
                 <Link to="/" className="nav-link">
-                  items
+                  product
                 </Link>
               </li>
             </ul>
